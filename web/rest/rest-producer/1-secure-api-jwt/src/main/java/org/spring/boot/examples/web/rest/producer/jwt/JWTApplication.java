4b4651a9060261c1c5ee41eee2b7733495380584
Amdouni Mohamed Ali <amdouni.mouhamed.ali@gmail.com>
2019-04-26 22:16:23 +0200

    [feat] add json collection to the test resources

--- web/rest/rest-producer/1-secure-api-jwt/src/main/java/org/spring/boot/examples/web/rest/producer/jwt/JWTApplication.java
+++ web/rest/rest-producer/1-secure-api-jwt/src/main/java/org/spring/boot/examples/web/rest/producer/jwt/JWTApplication.java
@@ -9,6 +9,7 @@ public class JWTApplication {
     public static void main(String[] args) {
 
         //source : https://www.callicoder.com/spring-boot-spring-security-jwt-mysql-react-app-part-2/
+        //you can find the postman collection of this project in the test resources
         SpringApplication.run(JWTApplication.class, args);
     }
 }

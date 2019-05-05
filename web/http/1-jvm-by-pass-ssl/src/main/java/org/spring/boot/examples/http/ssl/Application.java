1818e3f88beeaadbff1a8b3c66f4a0490b7b5e0f
Amdouni Mohamed Ali <amdouni.mouhamed.ali@gmail.com>
2019-05-05 10:18:19 +0200

    [feat] add jvm by pass ssl module

---
+++ web/http/1-jvm-by-pass-ssl/src/main/java/org/spring/boot/examples/http/ssl/Application.java
@@ -0,0 +1,15 @@
+package org.spring.boot.examples.http.ssl;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        // close the application context to shut down the custom ExecutorService
+        SpringApplication.run(Application.class, args).close();
+    }
+}

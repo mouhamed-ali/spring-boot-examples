2dbd09efe10edbf1d8a94c251ab258d94f267453
amdouni-mohamed-ali <amdouni.mouhamed.ali@gmail.com>
2019-05-20 13:30:17 +0200

    add a comment - difference between @Configuration and @Component

--- profiles/1-maven-profiles-example/src/main/java/org/spring/boot/examples/profiles/maven/config/MailServerConfig.java
+++ profiles/1-maven-profiles-example/src/main/java/org/spring/boot/examples/profiles/maven/config/MailServerConfig.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Component;
 @Component
 /*
  * you can use this annotation instead of @Configuration but it's a bad practise
+ * actually, the difference is : when using @Configuration + you declare some beans with @Bean annotation this beans will be cached.
+ * assume that you have this method [ @Bean demoBean(){ return new DemoBean();} ]. if you call this method many times in a @Configuration class you will
+ * have the same object but if you call it in a @Component class you will have a new object every time you call the method.
  */
 public class MailServerConfig {
 
